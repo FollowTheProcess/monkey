445d7a8019da52fcd9967b822c65ba5ce416d17f
Tom <tomfleet2018@gmail.com>
2021-11-27 15:05:51 +0000

    Working lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,45 +1,29 @@
 package lexer
 
-// Lexer is our semantic Lexer, it proceeds character by character
-// emitting tokens as it sees them
 type Lexer struct {
 	input        string
-	position     int  // Current position in input (points to current character)
-	readPosition int  // Current reading position in input (points to next character)
-	ch           byte // Current character under examination
+	position     int  // Current position in input (points to current char)
+	readPosition int  // Current reading position (points to next char)
+	ch           byte // Current char under examination
 }
 
-// New constructs and returns a new Lexer
-// the Lexer is initialised by reading the first rune in input
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar()
 	return l
 }
 
-// readChar reads the next character to be lexed or a 0 (ASCII "NUL")
-// if we have reached the end of the input stream
-//
-// If we are not at the end of the stream, the current character is set
-// and the current position and the position of the next character are advanced
 func (l *Lexer) readChar() {
-	// If we haven't read anything yet or if we've reached the end of the
-	// input stream, set current char to 0
-	// otherwise, index the input with current read position to grab the character
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
 		l.ch = l.input[l.readPosition]
 	}
 
-	// Advance our current and next positions
 	l.position = l.readPosition
 	l.readPosition += 1
 }
 
-// NextToken looks at the current character under evaluation and
-// returns the appropriate semantic Token for that character
-// in the case of 0 (ASCII "NUL"), it will return an EOF Token
 func (l *Lexer) NextToken() Token {
 	var token Token
 
@@ -69,7 +53,6 @@ func (l *Lexer) NextToken() Token {
 	return token
 }
 
-// newToken constructs and returns a Token
 func newToken(t TokenType, ch byte) Token {
 	return Token{Type: t, Literal: string(ch)}
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,59 +6,32 @@ func TestNextToken(t *testing.T) {
 	input := `=+(){},;`
 
 	tests := []struct {
-		name            string
 		expectedType    TokenType
 		expectedLiteral string
 	}{
-		{
-			name:            "assignment",
-			expectedType:    ASSIGN,
-			expectedLiteral: "=",
-		},
-		{
-			name:            "plus",
-			expectedType:    PLUS,
-			expectedLiteral: "+",
-		},
-		{
-			name:            "left parenthesis",
-			expectedType:    LPAREN,
-			expectedLiteral: "(",
-		},
-		{
-			name:            "right parenthesis",
-			expectedType:    RPAREN,
-			expectedLiteral: ")",
-		},
-		{
-			name:            "comma",
-			expectedType:    COMMA,
-			expectedLiteral: ",",
-		},
-		{
-			name:            "semicolon",
-			expectedType:    SEMICOLON,
-			expectedLiteral: ";",
-		},
-		{
-			name:            "end of file",
-			expectedType:    EOF,
-			expectedLiteral: "",
-		},
+		{ASSIGN, "="},
+		{PLUS, "+"},
+		{LPAREN, "("},
+		{RPAREN, ")"},
+		{LBRACE, "{"},
+		{RBRACE, "}"},
+		{COMMA, ","},
+		{SEMICOLON, ";"},
+		{EOF, ""},
 	}
 
 	l := New(input)
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			token := l.NextToken()
+		t.Run(string(tt.expectedType), func(t *testing.T) {
+			tok := l.NextToken()
 
-			if token.Type != tt.expectedType {
-				t.Fatalf("wrong token type, got %q, wanted %q", token.Type, tt.expectedType)
+			if tok.Type != tt.expectedType {
+				t.Fatalf("wrong token type: got %q, wanted %q", tok.Type, tt.expectedType)
 			}
 
-			if token.Literal != tt.expectedLiteral {
-				t.Fatalf("wrong token literal, got %q, wanted %q", token.Literal, tt.expectedLiteral)
+			if tok.Literal != tt.expectedLiteral {
+				t.Fatalf("wrong token literal: got %q, wanted %q", tok.Literal, tt.expectedLiteral)
 			}
 		})
 	}
